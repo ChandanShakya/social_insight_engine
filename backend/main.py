33007906294b0ba35f166d9a9d88896e0fbb4156
kabilraya <roykabil99@gmail.com>
2026-01-03 13:41:02 +0545

    Feat(app):modification of bothe frontend and backend logics

--- backend/main.py
+++ backend/main.py
@@ -1,32 +1,28 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+#main api endpoint
+
 from controllers.classify import classify_comments
 from pydantic import BaseModel
+from fastapi import FastAPI
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 class PostRequest(BaseModel):
-    post_id: str
+    post_id : str #for safe conversion
 
 @app.post("/scrape")
-def scrape_comments(data: PostRequest):
-    return {
-        "message": "Comments scraped successfully",
-        "total_comments": 45
+def scrape_comments(data:PostRequest):
+    print("Here goes the scraper code")
+
+    return{
+        "message" : "Comments scraped successfully",
+        "total_comments" : 45
     }
 
 @app.get("/classify")
-def get_classification(post_id: str):
+def get_classification():
     result = classify_comments()
+
     return {
-        "postId": post_id,
-        **result
+        "message":"Sentiment classification completed",
+        "data" : result
     }
