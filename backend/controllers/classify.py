908cf7cb412d199e3197aab49e00f5767902b823
kabilraya <roykabil99@gmail.com>
2026-01-03 11:31:41 +0545

    added the api endpoints for POST and GET

--- backend/controllers/classify.py
+++ backend/controllers/classify.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from transformers import pipeline
-
+import os
 #loading the model as a pipeline
 #here the first parameter is the able_task that defines what the model is capable of
 
@@ -8,7 +8,9 @@ from transformers import pipeline
 sentiment_pipeline = pipeline("sentiment-analysis", model="finiteautomata/bertweet-base-sentiment-analysis")
 
 def classify_comments():
-    df = pd.read_excel("comments.xlsx")
+    base_name = os.path.abspath(os.path.dirname(__file__))
+    file_name = os.path.abspath(os.path.join(base_name,"..","data","comments.xlsx"))
+    df = pd.read_excel(file_name)
 
     sentiments = []
 
@@ -18,8 +20,8 @@ def classify_comments():
 
     df["sentiments"] = sentiments
 
-    df.to_excel("comments.xlsx",index = False)
-    return df
+    df.to_excel(file_name,index = False)
+    return df.to_dict(orient = "records")
 
 def main():
     result = classify_comments()
--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,28 @@
 #main api endpoint
 
+from controllers.classify import classify_comments
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class PostRequest(BaseModel):
+    post_id : str #for safe conversion
+
+@app.post("/scrape")
+def scrape_comments(data:PostRequest):
+    print("Here goes the scraper code")
+
+    return{
+        "message" : "Comments scraped successfully",
+        "total_comments" : 45
+    }
+
+@app.get("/classify")
+def get_classification():
+    result = classify_comments()
+
+    return {
+        "message":"Sentiment classification completed",
+        "data" : result
+    }
